e5301ac93f8a31b5da8028c661b6d54b5ed08ea1
user <wow.lolomekiss@hotmail.fr>
2018-01-17 21:32:59 +0100

    jupyter final + fichier 30 31

---
+++ CIFAR10_3CONVNET_32_64_32_RELU_3_MAXPOOL_2_DROPOUT_S10_LR_1.py
@@ -0,0 +1,61 @@
+from keras.models import *
+from keras.layers import *
+from keras.optimizers import *
+from keras.activations import *
+from keras.losses import *
+from keras.datasets import *
+from keras.metrics import *
+from keras.callbacks import *
+import numpy as np
+import keras
+
+
+experiment_name="CIFAR10_3CONVNET_32_64_32_RELU_3_MAXPOOL_2_DROPOUT_S10_LR_1"
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+
+
+x_train = np.reshape(x_train,(-1,32,32,3))/255.0 
+x_test = np.reshape(x_test,(-1,32,32,3))/255.0
+
+
+y_train = keras.utils.to_categorical(y_train)
+y_test = keras.utils.to_categorical(y_test)
+
+print(x_train.shape)
+print(y_train.shape)
+
+tb_callback = TensorBoard("C:\\Users\jujuu\Documents\LogProjet\log" + experiment_name)
+
+model = Sequential()
+model.add(Conv2D(32,(3,3),padding='same',input_shape=(32,32,3)))
+model.add(Activation('relu'))
+model.add(MaxPool2D((2,2)))
+model.add(Dropout(0.20))
+
+model.add(Conv2D(64,(3,3),padding='same',input_shape=(32,32,3)))
+model.add(Activation('relu'))
+model.add(MaxPool2D((2,2)))
+model.add(Dropout(0.10))
+
+model.add(Conv2D(32,(3,3),padding='same',input_shape=(32,32,3)))
+model.add(Activation('relu'))
+model.add(MaxPool2D((2,2)))
+model.add(Dropout(0.20))
+
+
+model.add(Flatten())
+model.add(Dense(10, activation='sigmoid'))
+
+
+model.compile(sgd(lr=1,),
+                     mse, metrics=[categorical_accuracy])
+
+
+model.fit(x_train, y_train,
+                 batch_size= 2000,
+                 epochs=2000,
+                 verbose=1,
+                 callbacks=[tb_callback],
+                 validation_data=(x_test,y_test), )
